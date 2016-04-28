8d69185d4b8742f979d628eeb626988330695cfb
ZhiFeng Hu <hufeng1987@gmail.com>
2016-04-29 07:45:31 +0800

    sync code 2016-04-29
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- langtools/test/jdk/javadoc/doclet/testModules/module2/module-info.java
+++ langtools/test/jdk/javadoc/doclet/testModules/module2/module-info.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -21,14 +23,9 @@
  * questions.
  */
 
-
 /**
- * Check yield keyword is parsed and yield statement does nothing (yet).
- *
- * @test
- * @run
- */
-
-function func() {
-    yield 2;
+  * This is a test description for the module2 module.
+  */
+module module2 {
+    exports testpkgmdl2;
 }
