552605436f9594f728bac33b026acd19c0537fca
ZhiFeng Hu <hufeng1987@gmail.com>
2016-11-22 14:46:50 +0800

    sync code 2016-11-22
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- jdk/test/java/lang/instrument/modules/src/test/jdk/test/Main.java
+++ jdk/test/java/lang/instrument/modules/src/test/jdk/test/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,31 +21,23 @@
  * questions.
  */
 
-/*
- * @test
- * @bug 6657619
- * @modules jdk.naming.dns
- * @summary DnsContext.debug is public static mutable (findbugs)
- * @author Vincent Ryan
- */
+package jdk.test;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
-/*
- * Check that the 'debug' class member is no longer publicly accessible.
+/**
+ * Launched by AppendToClassPathModuleTest.
  */
-public class CheckAccess {
-    public static final void main(String[] args) throws Exception {
-        try {
-            Class clazz = Class.forName("com.sun.jndi.dns.DnsContext");
-            Field field = clazz.getField("debug");
-            if (Modifier.isPublic(field.getModifiers())) {
-                throw new Exception(
-                    "class member 'debug' must not be publicly accessible");
-            }
-        } catch (NoSuchFieldException e) {
-            // 'debug' is not publicly accessible, ignore exception
+public class Main {
+    public static void main(String... args) throws Exception {
+        // "java.class.path" system property is expected to be empty.
+        String value = System.getProperty("java.class.path");
+        if (!value.isEmpty()) {
+            throw new RuntimeException("Non-empty java.class.path=" + value);
+        }
+
+        // load the "hidden" class that should be loaded by the system loader
+        Class<?> c = Class.forName("ExampleForClassPath");
+        if (c.getClassLoader() != ClassLoader.getSystemClassLoader()) {
+            throw new RuntimeException(c + " loaderd by " + c.getClassLoader());
         }
     }
 }
--- jdk/test/java/lang/instrument/modules/src/test/module-info.java
+++ jdk/test/java/lang/instrument/modules/src/test/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,31 +21,5 @@
  * questions.
  */
 
-/*
- * @test
- * @bug 6657619
- * @modules jdk.naming.dns
- * @summary DnsContext.debug is public static mutable (findbugs)
- * @author Vincent Ryan
- */
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
-/*
- * Check that the 'debug' class member is no longer publicly accessible.
- */
-public class CheckAccess {
-    public static final void main(String[] args) throws Exception {
-        try {
-            Class clazz = Class.forName("com.sun.jndi.dns.DnsContext");
-            Field field = clazz.getField("debug");
-            if (Modifier.isPublic(field.getModifiers())) {
-                throw new Exception(
-                    "class member 'debug' must not be publicly accessible");
-            }
-        } catch (NoSuchFieldException e) {
-            // 'debug' is not publicly accessible, ignore exception
-        }
-    }
+module test {
 }
