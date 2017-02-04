a2f7bd42b767c299d0e6cba6f3c5d8f4f32f0874
huzhifeng <huzhifeng@douyu.tv>
2017-02-04 20:32:16 +0800

    sync code 2017-02-04
    Signed-off-by: huzhifeng <huzhifeng@douyu.tv>

--- langtools/src/java.compiler/share/classes/javax/lang/model/element/TypeElement.java
+++ langtools/src/java.compiler/share/classes/javax/lang/model/element/TypeElement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -35,8 +35,7 @@ import javax.lang.model.util.*;
  * type is a kind of class and an annotation type is a kind of
  * interface.
  *
- * <p> <a name="ELEM_VS_TYPE"></a>
- * While a {@code TypeElement} represents a class or interface
+ * <p> While a {@code TypeElement} represents a class or interface
  * <i>element</i>, a {@link DeclaredType} represents a class
  * or interface <i>type</i>, the latter being a use
  * (or <i>invocation</i>) of the former.
@@ -68,14 +67,14 @@ public interface TypeElement extends Element, Parameterizable, QualifiedNameable
      * the implicit {@code values} and {@code valueOf} methods of an
      * enum type.
      *
-     * <p> Note that as a particular instance of the {@linkplain
+     * @apiNote As a particular instance of the {@linkplain
      * javax.lang.model.element general accuracy requirements} and the
      * ordering behavior required of this interface, the list of
      * enclosed elements will be returned in the natural order for the
      * originating source of information about the type.  For example,
      * if the information about the type is originating from a source
      * file, the elements will be returned in source code order.
-     * (However, in that case the the ordering of synthesized
+     * (However, in that case the the ordering of {@linkplain Elements.Origin#MANDATED mandated}
      * elements, such as a default constructor, is not specified.)
      *
      * @return the enclosed elements in proper order, or an empty list if none
